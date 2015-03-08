a925cd46ecf35088bf598420e442f3aaf64ff9c8
radvieira <radvieira@gmail.com>
2015-03-07 19:01:32 -0700

    simple protractor test

--- src/test/vehicles-e2e-spec.js
+++ src/test/vehicles-e2e-spec.js
@@ -1,4 +1,17 @@
 /**
  * Created by radvieira on 15-03-07.
  */
-describe('Vehicles ')
+describe('Vehicles', function() {
+
+  'use strict';
+
+  it('should list vehicles', function() {
+
+    browser.get('http://localhost:3000/');
+
+    var vehicles = element.all(by.css('ul li'));
+    expect(vehicles.count()).toEqual(2);
+
+  });
+
+});
