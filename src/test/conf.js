fe47ce0c4446b25be85047c27898e5ff46827339
Raul Vieira <radvieira@gmail.com>
2015-03-13 18:03:46 -0400

    starting/stopping the web server on e2e start/stop

--- src/test/conf.js
+++ src/test/conf.js
@@ -1,6 +1,17 @@
+var exec = require('child_process').exec,
+    child;
 
-var child = require('child_process');
-child.exec('grunt connect:server');
+child = exec('grunt connect:server:keepalive', function(err, stdout, stderr) {
+  console.log(stdout);
+  console.log(stderr);
+  if (err) {
+    console.log(err);
+  }
+});
+
+process.on('exit', function() {
+  child.kill();
+});
 
 exports.config = {
   specs: ['**/*-e2e-spec.js'],
